e793f91d311a1e9c803046c1a819169da7081b72
Abdullah <abdullah.khan@gamil.com>
2025-06-11 14:41:48 +0500

    first commit

--- src/app/Helper/Httpsreponse.js
+++ src/app/Helper/Httpsreponse.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-export const HttpasAxios=axios.create({
-    baseURL:process.env.baseURL
-})
+export const HttpAxios = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL_DEV || process.env.REACT_APP_BASE_URL_PROD
+});
